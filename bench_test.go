6184290db9da6679fa55ea462aaf081c08e3006a
Tommi Virtanen <tv@eagain.net>
2015-03-31 19:29:22 -0700

    Make benchmarks more realistic
    For the stdlib tests these were based on, zero bytes still encode to
    something. Base58 and friends encode zero as no output, so this was
    just benchmarking a very expensive "skip zero bytes" loop.

--- bench_test.go
+++ bench_test.go
@@ -11,6 +11,7 @@ import (
 
 func BenchmarkBase58EncodeToString(b *testing.B) {
 	data := make([]byte, 8192)
+	data[0] = 0xff // without this, it's just skipping zero bytes
 	b.SetBytes(int64(len(data)))
 	for i := 0; i < b.N; i++ {
 		basen.Base58.EncodeToString(data)
@@ -18,15 +19,18 @@ func BenchmarkBase58EncodeToString(b *testing.B) {
 }
 
 func BenchmarkBase58DecodeString(b *testing.B) {
-	data := basen.Base58.EncodeToString(make([]byte, 8192))
-	b.SetBytes(int64(len(data)))
+	data := make([]byte, 8192)
+	data[0] = 0xff // without this, it's just skipping zero bytes
+	s := basen.Base58.EncodeToString(data)
+	b.SetBytes(int64(len(s)))
 	for i := 0; i < b.N; i++ {
-		basen.Base58.DecodeString(data)
+		basen.Base58.DecodeString(s)
 	}
 }
 
 func BenchmarkBase62EncodeToString(b *testing.B) {
 	data := make([]byte, 8192)
+	data[0] = 0xff // without this, it's just skipping zero bytes
 	b.SetBytes(int64(len(data)))
 	for i := 0; i < b.N; i++ {
 		basen.Base62.EncodeToString(data)
@@ -34,9 +38,11 @@ func BenchmarkBase62EncodeToString(b *testing.B) {
 }
 
 func BenchmarkBase62DecodeString(b *testing.B) {
-	data := basen.Base62.EncodeToString(make([]byte, 8192))
-	b.SetBytes(int64(len(data)))
+	data := make([]byte, 8192)
+	data[0] = 0xff // without this, it's just skipping zero bytes
+	s := basen.Base62.EncodeToString(data)
+	b.SetBytes(int64(len(s)))
 	for i := 0; i < b.N; i++ {
-		basen.Base62.DecodeString(data)
+		basen.Base62.DecodeString(s)
 	}
 }
